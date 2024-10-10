c2ff2787e00bbd6048a254b052373dc055ed3118
Soheil <soheilzarepoor79@gmail.com>
2024-10-10 10:58:54 +0330

    revert to old drizzle connector

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,16 @@
-import { drizzle } from "drizzle-orm/connect";
+// TODO: use new drizzle/connect
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import { env } from "~/env";
 
 import * as schema from "./schema/index";
 
-const db = await drizzle("postgres-js", {
-  schema,
-  connection: { url: env.DATABASE_URL },
+export const client = postgres(env.DATABASE_URL, {
+  max: env.DB_MIGRATING ? 1 : undefined,
 });
-
+const db = drizzle(client, {
+  schema,
+});
 export default db;
